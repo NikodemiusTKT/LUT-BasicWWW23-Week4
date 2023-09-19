4da32257568abf002e496f1ed346dfbff925cd97
Teemu Tanninen <teemu.tanninen@student.lut.fi>
2023-09-19 17:12:09 +0300

    Little refactoring done

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const municipalityDataUrl =
-  'https://statfin.stat.fi/PxWeb/sq/4e244893-7761-4c4f-8e55-7a8d41d86eff';
-const employmentDataURL =
-  'https://statfin.stat.fi/PxWeb/sq/5e288b40-f8c8-4f1e-b3b0-61b86ce5c065';
+window.onload = async () => {
+  const municipalityDataUrl =
+    'https://statfin.stat.fi/PxWeb/sq/4e244893-7761-4c4f-8e55-7a8d41d86eff';
+  const employmentDataURL =
+    'https://statfin.stat.fi/PxWeb/sq/5e288b40-f8c8-4f1e-b3b0-61b86ce5c065';
+  const munPopData = await fetchJsonData(municipalityDataUrl);
+  const employData = await fetchJsonData(employmentDataURL);
+    fillTableWithData(munPopData, employData);
+};
 
 async function fetchJsonData(url) {
   try {
@@ -20,11 +25,12 @@ async function fetchJsonData(url) {
     );
   }
 }
+
 function fillTableWithData(munPopData, employData) {
   const municipality = munPopData.dataset.dimension.Alue.category.label;
   const population = munPopData.dataset.value;
   const employment = employData.dataset.value;
-  let fragment = document.createDocumentFragment();
+  let tRowArray = new Array();
   Object.values(municipality).forEach((mun, index) => {
     const curPop = population[index];
     const curEmp = employment[index];
@@ -32,23 +38,18 @@ function fillTableWithData(munPopData, employData) {
     const roundedEmployRate = (employRate * 100).toFixed(2);
     let tRow = createTableRowElem(mun, curPop, curEmp, `${roundedEmployRate}%`);
     if (employRate > 0.45) tRow.style.backgroundColor = '#abffbd';
-    if (employRate < 0.25) tRow.style.backgroundColor = '#ff9e9e';
-    fragment.appendChild(tRow);
+    else if (employRate < 0.25) tRow.style.backgroundColor = '#ff9e9e';
+    tRowArray.push(tRow);
   });
-  document.querySelector('tbody').appendChild(fragment);
+  document.querySelector('tbody').append(...tRowArray);
 }
+
 function createTableRowElem(...textArgs) {
   let tRow = document.createElement('tr');
   for (let arg of textArgs) {
     let tCol = document.createElement('td');
     tCol.innerText = arg;
-    tRow.appendChild(tCol);
+    tRow.append(tCol);
   }
   return tRow;
 }
-
-window.onload = async () => {
-  const munPopData = await fetchJsonData(municipalityDataUrl);
-  const employData = await fetchJsonData(employmentDataURL);
-  fillTableWithData(munPopData, employData);
-};
